4d7fbab18db69e75937179d434f5ab0d8a0272fa
yhsin79 <yhsin79@naver.com>
2021-04-27 18:03:22 +0900

    #04 오늘날짜출력문추가 -ys-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,10 +1,14 @@
 package com.git.main;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("안녕 git~ 반가워~ 오랜만이야!!");
-
+		
+		Date today=new Date();
+		System.out.println(today);
 	}
 
 }
