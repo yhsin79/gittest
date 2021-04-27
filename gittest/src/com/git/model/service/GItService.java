69c74468b13de0169b63f76dc5ca7ef0738e9fd5
yhsin79 <yhsin79@naver.com>
2021-04-27 19:37:10 +0900

    #02 service에seachName추가 -ys-

--- gittest/src/com/git/model/service/GItService.java
+++ gittest/src/com/git/model/service/GItService.java
@@ -2,9 +2,8 @@ package com.git.model.service;
 
 public class GItService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void searchName() {
+		
 	}
 
 }
