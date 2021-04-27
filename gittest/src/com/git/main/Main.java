2d1915b3a38f79feccf298885280dcdffc9cb93f
yhsin79 <yhsin79@naver.com>
2021-04-27 19:25:52 +0900

    #01 main에 출력문추가 -ys-

--- gittest/src/com/git/main/Main.java
+++ gittest/src/com/git/main/Main.java
@@ -1,14 +1,10 @@
 package com.git.main;
 
-import java.util.Date;
-
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕 git~ 반가워~ 오랜만이야!!");
-		
-		Date today=new Date();
-		System.out.println(today);
+		System.out.println("난 YS브런치야!");
+
 	}
 
 }
